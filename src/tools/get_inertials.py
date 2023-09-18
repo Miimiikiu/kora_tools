facd7fe1e9a176162f0e4a8976a5111134357e55
Miimiikiu <miimiikiu@gmail.com>
2023-09-18 10:31:36 -0700

    added mass defaults to get_inertials.py

--- src/tools/get_inertials.py
+++ src/tools/get_inertials.py
@@ -10,13 +10,18 @@ This software is licensed under the MIT License.
 import pymeshlab
 import os
 
+### EDIT HERE ###
 
-mesh_path = '/home/sunset/sunset_ws/src/kora/src/kora_desc/OBJ/'
+mesh_path = '/home/sunset/sunset_ws/src/kora/src/kora_desc/visuals/OBJ/'
+mesh_type = 'obj'
+
+#################
+
 with open(mesh_path + 'geometry/geometry.csv', 'w') as outfile:
-    outfile.write('Mesh Name;Inertia Tensor;Center of Mass\n') # Separating with ;
+    outfile.write('Mesh Name;Inertia Tensor;Center of Mass;Mass\n') # Separating with ;
     for item in os.listdir(mesh_path):
         
-        if item[-3:] == 'obj':
+        if item[-3:] == mesh_type:
             print(item)
             ms = pymeshlab.MeshSet()
             ms.load_new_mesh(mesh_path + item)
@@ -32,5 +37,6 @@ with open(mesh_path + 'geometry/geometry.csv', 'w') as outfile:
             com = str(com).replace('  ', ' ')
             com = com.replace(' ', ',')
             
-            outfile.write('{};{};{}\n'.format(item, inertia, com))
+            
+            outfile.write('{};{};{};1\n'.format(item, inertia, com))
             #print('inertia tensor: {}\ncenter of mass: {}\n\n'.format(inertia, com))
